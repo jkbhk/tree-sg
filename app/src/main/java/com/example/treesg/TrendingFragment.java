1c34f84fd9e68ca394fc49eb497f7ec964d27036
Looi Yue Peng <marcusheadgotscarm16@gmail.com>
2021-09-18 03:42:12 +0800

    added basic navigation bar

---
+++ app/src/main/java/com/example/treesg/TrendingFragment.java
@@ -0,0 +1,36 @@
+package com.example.treesg;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrendingFragment extends Fragment {
+
+    private TrendingViewModel mViewModel;
+
+    public static TrendingFragment newInstance() {
+        return new TrendingFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.trending_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TrendingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/treesg/TrendingViewModel.java
@@ -0,0 +1,7 @@
+package com.example.treesg;
+
+import androidx.lifecycle.ViewModel;
+
+public class TrendingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
