d79cacb9d33e9a1c0deff9b65608cce1a932720e
xzijian <xiezijian99@gmail.com>
2021-10-18 19:51:25 +0800

    Updating Setting classes

--- app/src/main/java/com/example/treesg/ui/settings/SettingsPrivacy.java
+++ app/src/main/java/com/example/treesg/ui/settings/SettingsPrivacy.java
@@ -1,16 +1,59 @@
 package com.example.treesg.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.treesg.R;
 
 public class SettingsPrivacy extends AppCompatActivity {
+    EditText Curr, New, Confirm;
+    Button ConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.settings_privacy);
+        Curr = findViewById(R.id.CurrentPassword);
+        New = findViewById(R.id.NewPassword);
+        Confirm = findViewById(R.id.ConfirmPassword);
+        ConfirmButton = findViewById(R.id.button8);
+
+        ConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentPassword = Curr.getText().toString().trim();
+                String newPassword = New.getText().toString().trim();
+                String confirmPassword = Confirm.getText().toString().trim();
+
+                if(TextUtils.isEmpty(currentPassword)){
+                    Curr.setError("Current Password is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(newPassword)){
+                    New.setError("New Password is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(confirmPassword)){
+                    Confirm.setError("Confirm New Password is required");
+                    return;
+                }
+                if(newPassword.length() < 8){
+                    New.setError("Password set must be at least 8 characters.");
+                    return;
+                }
+                if(!(newPassword.equals(confirmPassword))){
+                    Confirm.setError("Confirm New Password must match New Password.");
+                    return;
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/treesg/ui/settings/SettingsProfileDetails.java
+++ app/src/main/java/com/example/treesg/ui/settings/SettingsProfileDetails.java
@@ -1,15 +1,33 @@
 package com.example.treesg.ui.settings;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.treesg.R;
 
 public class SettingsProfileDetails extends AppCompatActivity {
+    Button Save;
+    EditText textName, textUsername, textDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_profiledetails);
+
+        Save = findViewById(R.id.button9);
+        textName = findViewById(R.id.Name);
+        textUsername = findViewById(R.id.Username);
+        textDescription = findViewById(R.id.Description);
+
+        String Name = textName.getText().toString().trim();
+        String Username = textUsername.getText().toString().trim();
+        String Description = textDescription.getText().toString();
+
+
     }
 }
