311481a0aa9e7917cb4b488ec51a73b039cb498e
hermeess <hermeskd35@gmail.com>
2021-10-29 23:31:28 +0800

    update for user announcements

--- app/src/main/java/com/example/treesg/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/treesg/ui/home/HomeFragment.java
@@ -1,24 +1,44 @@
 package com.example.treesg.ui.home;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.treesg.MainActivity;
+import com.example.treesg.Announcement;
+import com.example.treesg.AnnouncementAdapter;
 import com.example.treesg.R;
 import com.example.treesg.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
-    private Button annButton;
+    private RecyclerView mRecyclerView;
+    private AnnouncementAdapter mAdapter;
+
+
+    private ArrayList<Announcement> mAnns = null;
+    FirebaseFirestore db;
+    ProgressDialog progressDialog;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,17 +59,55 @@ public class HomeFragment extends Fragment {
         });*/
         return root;
     }
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-        annButton = view.findViewById(R.id.annPostButton);
 
-        annButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.navigationController.navigate(R.id.navigation_userAnnouncement);
-            }
-        });
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data...");
+        progressDialog.show();
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.ann_recycler_view1);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        db = FirebaseFirestore.getInstance();
+        mAnns = new ArrayList<Announcement>();
+        mAdapter = new AnnouncementAdapter(getContext(), mAnns);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        EventChangeListener();
+    }
+
+    private void EventChangeListener() {
+        db.collection("Announcement")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        }
+                        for (DocumentChange dc: value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                mAnns.add(dc.getDocument().toObject(Announcement.class));
+                            }
+
+                            mAdapter.notifyDataSetChanged();
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+                        }
+                    }
+                });
     }
     @Override
     public void onDestroyView() {
