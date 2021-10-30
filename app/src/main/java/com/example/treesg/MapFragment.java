3d8c70549fbb37a574846fa305b36e2ff8463f65
Xavier <xaviergohzr@outlook.com>
2021-10-30 19:58:04 +0800

    Added location service

--- app/src/main/java/com/example/treesg/MapFragment.java
+++ app/src/main/java/com/example/treesg/MapFragment.java
@@ -1,10 +1,15 @@
 package com.example.treesg;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.PointF;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -16,6 +21,7 @@ import android.widget.Toast;
 
 import com.google.firebase.database.core.utilities.Tree;
 import com.google.gson.JsonElement;
+import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
@@ -47,6 +53,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -58,6 +65,7 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
     private MapboxMap mapboxMap;
     static StringBuilder builder = new StringBuilder();
     private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
 
 
     @Override
@@ -73,12 +81,8 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
         }
 
 
-
-
-
-
         // Inflate the layout for this fragment
-        Mapbox.getInstance(getContext().getApplicationContext(),"pk.eyJ1IjoiamFsYW5ldW5vcyIsImEiOiJja3Y4YWFid3U0Zzl2MzJwNmg5b3RhNmRyIn0.uQZju4_sVA9GdF9lBT4Rgg");
+        Mapbox.getInstance(getContext().getApplicationContext(), "pk.eyJ1IjoiamFsYW5ldW5vcyIsImEiOiJja3Y4YWFid3U0Zzl2MzJwNmg5b3RhNmRyIn0.uQZju4_sVA9GdF9lBT4Rgg");
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         // Mapbox access token is configured here. This needs to be called either in your application
         // object or in the same activity which contains the mapview.
@@ -92,8 +96,6 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        Treedebugger.log("Calling within onstyle loaded");
-                        enableLocationComponent(style);
                         mapboxMap.addOnMapClickListener(MapFragment.this);
 
                         VectorSource vectorSource = new VectorSource("vector-source", "mapbox://jalaneunos.70mhubzq");
@@ -106,15 +108,25 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
 
                         style.addLayer(circleLayer);
 
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-                        CameraPosition cameraPosition = new CameraPosition.Builder()
-                                .target(new LatLng(1.3521, 103.8198)) // set the camera's center position
-                                .zoom(10.5f)  // set the camera's zoom level
-                                .tilt(20)  // set the camera's tilt
-                                .build();
-                        // Move the camera to that position
-                        mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        enableLocationComponent(style);
 
+                        CameraPosition userLocation = mapboxMap.getCameraPosition();
+
+
+                        CameraPosition startPosition  = new CameraPosition.Builder()
+                                .target(new LatLng(1.3485, 103.7156))
+                                .zoom(6f)
+                                .build();
+
+                        mapboxMap.setCameraPosition(startPosition);
+
+
+                        CameraPosition zoomedPosition  = new CameraPosition.Builder()
+                                .target(new LatLng(1.3485, 103.7156))
+                                .zoom(17f)
+                                .build();
+
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(zoomedPosition), 7000);
 
                     }
 
@@ -143,15 +155,20 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
 
         List<Feature> features = mapboxMap.queryRenderedFeatures(pixel);
 
+
         // Get the first feature within the list if one exist
         if (features.size() > 0) {
             Feature feature = features.get(0);
 
             // Ensure the feature has properties defined
             if (feature.properties() != null) {
+                int j = 0;
                 for (Map.Entry<String, JsonElement> entry : feature.properties().entrySet()) {
+                    j += 1;
                     // Log all the properties
                     Log.d(TAG, String.format("%s = %s", entry.getKey(), entry.getValue()));
+                    Treedebugger.log(String.valueOf(entry.getKey()) + String.valueOf(entry.getValue()));
+
                     if (entry.getKey().equals("species_id")) {
                         String species_id = String.valueOf(entry.getValue());
                         species_id = species_id.substring(1, species_id.length() - 2);
@@ -173,30 +190,35 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
                     }
                     args.putString(entry.getKey(), String.valueOf(entry.getValue()));
                 }
-                TreeDialog dialog = new TreeDialog();
-                dialog.setArguments(args);
-                dialog.setTargetFragment(MapFragment.this, 1);
-                dialog.show(getParentFragmentManager(), "TreeDialog");
-                return true;
+                if (j == 10) {
+                    TreeDialog dialog = new TreeDialog();
+                    dialog.setArguments(args);
+                    dialog.setTargetFragment(MapFragment.this, 1);
+                    dialog.show(getParentFragmentManager(), "TreeDialog");
+                    return true;
+                }
             }
-
-
         }
         return false;
     }
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-        Treedebugger.log("Calling within enable location ");
 
-        LocationComponentOptions locationComponentOptions = LocationComponentOptions.builder(getContext())
-                .layerBelow("circle-layer-id")
-                .foregroundDrawable(R.drawable.heart)
-                .build();
+
+
 
         // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
-            Treedebugger.log("Calling within permissions granted ");
+
+            LocationComponentOptions customLocationComponentOptions = LocationComponentOptions.builder(getContext())
+                    .elevation(5)
+                    .pulseEnabled(true)
+                    .pulseAlpha(Color.BLUE)
+                    .pulseMaxRadius(200)
+                    .accuracyAlpha(.0f)
+                    .build();
+
 
 
             // Get an instance of the component
@@ -205,18 +227,34 @@ public class MapFragment extends Fragment implements MapboxMap.OnMapClickListene
         // Activate with options
             locationComponent.activateLocationComponent(
                     LocationComponentActivationOptions.builder(getContext(), loadedMapStyle)
-                            .locationComponentOptions(locationComponentOptions)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .useDefaultLocationEngine(true)
                             .build());
+
 
         // Enable to make component visible
             locationComponent.setLocationComponentEnabled(true);
 
+
+
+
         // Set the component's camera mode
             locationComponent.setCameraMode(CameraMode.TRACKING);
 
+
+
+
+
+
         // Set the component's render mode
             locationComponent.setRenderMode(RenderMode.COMPASS);
-            Treedebugger.log("All location functions complete ");
+
+
+
+
+
+
+
 
         } else {
             permissionsManager = new PermissionsManager(this);
