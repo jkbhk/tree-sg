50648fdf04a64d8342f26ae020f7b177da5ea598
cheezihoe <cheezihoe@gmail.com>
2021-10-30 14:36:20 +0800

    added treesg mission/counter

--- app/src/main/java/com/example/treesg/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/treesg/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,8 +20,9 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    private WebView webView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    /*public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
@@ -28,15 +30,37 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+       final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        final WebView webView = binding.webview;
+        notificationsViewModel.
+
+
         return root;
+    }*/
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        String url = "https://www.nparks.gov.sg/treessg/one-million-trees-movement";
+        WebView view = (WebView) rootView.findViewById(R.id.webview);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+
+        return rootView;
     }
+
+
+
+
 
     @Override
     public void onDestroyView() {
