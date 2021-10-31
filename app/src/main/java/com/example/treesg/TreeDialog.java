3cc1c37451ee6280af3b8615bed30aa45c00e4a1
Xavier <xaviergohzr@outlook.com>
2021-10-31 20:28:35 +0800

    Commiting soemthing

--- app/src/main/java/com/example/treesg/TreeDialog.java
+++ app/src/main/java/com/example/treesg/TreeDialog.java
@@ -1,8 +1,15 @@
 package com.example.treesg;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,26 +17,46 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.Distribution;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.collection.LLRBNode;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class TreeDialog extends DialogFragment {
 
     private static final String TAG = "TreeDialog";
+    private static final int GET_FROM_GALLERY = 3;
     private TextView mActionCancel;
 
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.tree_dialog, container, false);
         mActionCancel = view.findViewById(R.id.txtclose);
         TextView speciesName = view.findViewById(R.id.speciesName);
@@ -40,6 +67,11 @@ public class TreeDialog extends DialogFragment {
         Button hugTree = view.findViewById(R.id.hug_tree);
         Button makePost = view.findViewById(R.id.make_post);
         TextView pointsEarned = view.findViewById(R.id.points_earned);
+        ImageButton imageButton = view.findViewById(R.id.imageButton);
+        LinearLayout information = view.findViewById(R.id.information);
+        LinearLayout inputs = view.findViewById(R.id.inputs);
+        LinearLayout bottom = view.findViewById(R.id.bottom);
+
 
 
         Bundle mArgs = getArguments();
@@ -72,6 +104,7 @@ public class TreeDialog extends DialogFragment {
         hugTree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                UserManager.instance.incrementPointsAsync(20,null);
                 hugTree.setBackgroundColor(Color.GRAY);
                 hugTree.setEnabled(false);
                 pointsEarned.setVisibility(View.VISIBLE);
@@ -79,8 +112,25 @@ public class TreeDialog extends DialogFragment {
             }
         });
 
-        return view;
+        makePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                information.setVisibility(View.GONE);
+                bottom.setVisibility(View.GONE);
+                inputs.setVisibility(View.VISIBLE);
+            }
+        });
 
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+            }
+        });
+
+
+
+        return view;
 
     }
 
